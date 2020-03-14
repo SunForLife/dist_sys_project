24ab5c092666b5f597a6233eafe149642210b2d1
Danil Geydebreht <danil.geydebreht@gmail.com>
2020-03-14 13:01:01 +0300

    Multiple request handlers

--- main.go
+++ main.go
@@ -14,6 +14,10 @@ func main() {
 	onlineShopHandler := OnlineShopHandler{Products: make([]Product, 0)}
 
 	log.Printf("Started on port: %d\n", *port)
-	http.HandleFunc("/", onlineShopHandler.handler) // each request calls handler
+	http.HandleFunc("/get-product-list", onlineShopHandler.handlerGetProductList)
+	http.HandleFunc("/get-product-info", onlineShopHandler.handlerGetProductInfo)
+	http.HandleFunc("/create-new-product", onlineShopHandler.handlerCreateNewProduct)
+	http.HandleFunc("/change-product-by-name", onlineShopHandler.handlerChangeProductByName)
+	http.HandleFunc("/delete-product", onlineShopHandler.handlerDeleteProduct)
 	log.Fatal(http.ListenAndServe("0.0.0.0:"+strconv.Itoa(*port), nil))
 }
--- online_shop_handler.go
+++ online_shop_handler.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
 )
 
 // Describes Product.
@@ -20,136 +19,122 @@ type OnlineShopHandler struct {
 	Products []Product
 }
 
-// handler method of OnlineShopHandler.
-func (osh *OnlineShopHandler) handler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		if strings.Contains(r.RequestURI, "get-product-list") {
-			log.Println("Got get-product-list request")
+// GetProductList handler method of OnlineShopHandler.
+func (osh *OnlineShopHandler) handlerGetProductList(w http.ResponseWriter, r *http.Request) {
+	log.Println("Got get-product-list request")
 
-			json, err := json.Marshal(osh.Products)
+	json, err := json.Marshal(osh.Products)
+	if err != nil {
+		ErrorRequest(w, err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintln(w, string(json))
+}
+
+// GetProductInfo handler method of OnlineShopHandler.
+func (osh *OnlineShopHandler) handlerGetProductInfo(w http.ResponseWriter, r *http.Request) {
+	log.Println("Got get-product-info request")
+
+	if len(r.URL.Query()["name"]) == 0 {
+		BadRequest(w, "name param not found")
+		return
+	}
+	name := r.URL.Query()["name"][0]
+
+	for _, product := range osh.Products {
+		if product.Name == name {
+			json, err := json.Marshal(product)
 			if err != nil {
 				ErrorRequest(w, err)
 				return
 			}
 			w.Header().Set("Content-Type", "application/json")
 			fmt.Fprintln(w, string(json))
-
-		} else if strings.Contains(r.RequestURI, "get-product-info") {
-			log.Println("Got get-product-info request")
-
-			if len(r.URL.Query()["name"]) == 0 {
-				BadRequest(w, "name param not found")
-				return
-			}
-			name := r.URL.Query()["name"][0]
-
-			for _, product := range osh.Products {
-				if product.Name == name {
-					json, err := json.Marshal(product)
-					if err != nil {
-						ErrorRequest(w, err)
-						return
-					}
-					w.Header().Set("Content-Type", "application/json")
-					fmt.Fprintln(w, string(json))
-					return
-				}
-			}
-
-			NotFoundRequest(w, fmt.Sprint("Not found product with name:", name))
-
-		} else {
-			BadRequest(w, "Got unexpected GET request")
+			return
 		}
-		return
-
-	case "POST":
-		if strings.Contains(r.RequestURI, "create-new-product") {
-			log.Println("Got create-new-product request")
-
-			if len(r.URL.Query()["name"]) == 0 {
-				BadRequest(w, "name param not found")
-				return
-			}
-			name := r.URL.Query()["name"][0]
-			if len(r.URL.Query()["code"]) == 0 {
-				BadRequest(w, "code param not found")
-				return
-			}
-			code := r.URL.Query()["code"][0]
-			if len(r.URL.Query()["category"]) == 0 {
-				BadRequest(w, "category param not found")
-				return
-			}
-			category := r.URL.Query()["category"][0]
-
-			osh.Products = append(osh.Products, Product{Name: name, Code: code, Category: category})
-
-		} else if strings.Contains(r.RequestURI, "change-product-by-name") {
-			log.Println("Got change-product-by-name request")
-
-			if len(r.URL.Query()["old-name"]) == 0 {
-				BadRequest(w, "old-name param not found")
-				return
-			}
-			oldName := r.URL.Query()["old-name"][0]
-			if len(r.URL.Query()["name"]) == 0 {
-				BadRequest(w, "name param not found")
-				return
-			}
-			name := r.URL.Query()["name"][0]
-			if len(r.URL.Query()["code"]) == 0 {
-				BadRequest(w, "code param not found")
-				return
-			}
-			code := r.URL.Query()["code"][0]
-			if len(r.URL.Query()["category"]) == 0 {
-				BadRequest(w, "category param not found")
-				return
-			}
-			category := r.URL.Query()["category"][0]
-
-			for i := range osh.Products {
-				if osh.Products[i].Name == oldName {
-					osh.Products[i] = Product{Name: name, Code: code, Category: category}
-					return
-				}
-			}
-
-			NotFoundRequest(w, fmt.Sprint("Not found product with name:", name))
-
-		} else {
-			BadRequest(w, "Got unexpected POST request")
-		}
-		return
-
-	case "DELETE":
-		if strings.Contains(r.RequestURI, "delete-product") {
-			log.Println("Got delete-product request")
-
-			if len(r.URL.Query()["name"]) == 0 {
-				BadRequest(w, "name param not found")
-				return
-			}
-
-			name := r.URL.Query()["name"][0]
-			for i := range osh.Products {
-				if osh.Products[i].Name == name {
-					osh.Products[i] = osh.Products[len(osh.Products)-1]
-					osh.Products = osh.Products[:len(osh.Products)-1]
-					return
-				}
-			}
-
-			NotFoundRequest(w, fmt.Sprint("Not found product with name:", name))
-
-		} else {
-			BadRequest(w, "Got unexpected DELETE request")
-		}
-		return
-
 	}
+
+	NotFoundRequest(w, fmt.Sprint("Not found product with name:", name))
+}
+
+// CreateNewProduct handler method of OnlineShopHandler.
+func (osh *OnlineShopHandler) handlerCreateNewProduct(w http.ResponseWriter, r *http.Request) {
+	log.Println("Got create-new-product request")
+
+	if len(r.URL.Query()["name"]) == 0 {
+		BadRequest(w, "name param not found")
+		return
+	}
+	name := r.URL.Query()["name"][0]
+	if len(r.URL.Query()["code"]) == 0 {
+		BadRequest(w, "code param not found")
+		return
+	}
+	code := r.URL.Query()["code"][0]
+	if len(r.URL.Query()["category"]) == 0 {
+		BadRequest(w, "category param not found")
+		return
+	}
+	category := r.URL.Query()["category"][0]
+
+	osh.Products = append(osh.Products, Product{Name: name, Code: code, Category: category})
+}
+
+// ChangeProductByName handler method of OnlineShopHandler.
+func (osh *OnlineShopHandler) handlerChangeProductByName(w http.ResponseWriter, r *http.Request) {
+	log.Println("Got change-product-by-name request")
+
+	if len(r.URL.Query()["old-name"]) == 0 {
+		BadRequest(w, "old-name param not found")
+		return
+	}
+	oldName := r.URL.Query()["old-name"][0]
+	if len(r.URL.Query()["name"]) == 0 {
+		BadRequest(w, "name param not found")
+		return
+	}
+	name := r.URL.Query()["name"][0]
+	if len(r.URL.Query()["code"]) == 0 {
+		BadRequest(w, "code param not found")
+		return
+	}
+	code := r.URL.Query()["code"][0]
+	if len(r.URL.Query()["category"]) == 0 {
+		BadRequest(w, "category param not found")
+		return
+	}
+	category := r.URL.Query()["category"][0]
+
+	for i := range osh.Products {
+		if osh.Products[i].Name == oldName {
+			osh.Products[i] = Product{Name: name, Code: code, Category: category}
+			return
+		}
+	}
+
+	NotFoundRequest(w, fmt.Sprint("Not found product with name:", name))
+}
+
+// DeleteProduct handler method of OnlineShopHandler.
+func (osh *OnlineShopHandler) handlerDeleteProduct(w http.ResponseWriter, r *http.Request) {
+	log.Println("Got delete-product request")
+
+	if len(r.URL.Query()["name"]) == 0 {
+		BadRequest(w, "name param not found")
+		return
+	}
+
+	name := r.URL.Query()["name"][0]
+	for i := range osh.Products {
+		if osh.Products[i].Name == name {
+			osh.Products[i] = osh.Products[len(osh.Products)-1]
+			osh.Products = osh.Products[:len(osh.Products)-1]
+			return
+		}
+	}
+
+	NotFoundRequest(w, fmt.Sprint("Not found product with name:", name))
 }
 
 func BadRequest(w http.ResponseWriter, err string) {
